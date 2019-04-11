de55ed47a0c785b1d54abe89875a12f5368ef1a0
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-11 02:52:21 -0700

    update 115.unique-paths-ii.cpp

---
+++ 115.unique-paths-ii.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=lintcode id=115 lang=cpp
+ *
+ * [115] Unique Paths II
+ *
+ * https://www.lintcode.com/problem/unique-paths-ii/description
+ *
+ * lintcode
+ * Easy (31.00%)
+ * Total Accepted:    19352
+ * Total Submissions: 62403
+ * Testcase Example:  '[[0]]'
+ *
+ * Follow up for "Unique Paths":
+ * 
+ * Now consider if some obstacles are added to the grids. How many unique paths
+ * would there be?
+ * 
+ * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ */
+class Solution {
+public:
+    /**
+     * @param obstacleGrid: A list of lists of integers
+     * @return: An integer
+     */
+    int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
+        if (obstacleGrid.empty() || obstacleGrid[0].empty() || obstacleGrid[0][0] == 1) return 0;
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<long> dp(n, 0);
+        dp[0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (obstacleGrid[i][j] == 1) dp[j] = 0;
+                else if (j > 0) dp[j] += dp[j - 1];
+            }
+        }   
+        return dp[n - 1];
+    }
+};
