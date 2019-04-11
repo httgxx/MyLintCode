ee07c8bf8ee217e44b11f0e96026f815a0ecb870
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-11 02:17:34 -0700

    update 116.jump-game.cpp

---
+++ 116.jump-game.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=lintcode id=116 lang=cpp
+ *
+ * [116] Jump Game
+ *
+ * https://www.lintcode.com/problem/jump-game/description
+ *
+ * lintcode
+ * Medium (39.00%)
+ * Total Accepted:    24955
+ * Total Submissions: 62868
+ * Testcase Example:  '[0,8,2,0,9]'
+ *
+ * Given an array of non-negative integers, you are initially positioned at the
+ * first index of the array.
+ * Each element in the array represents your maximum jump length at that
+ * position. 
+ * 
+ * Determine if you are able to reach the last index.
+ *
+ * @Category DP
+ * @Ideas
+ *
+ */
+class Solution {
+public:
+    /**
+     * @param A: A list of integers
+     * @return: A boolean
+     */
+    bool canJump(vector<int> &A) {
+        vector<int> dp(A.size(), 0);
+        for (int i = 1; i < A.size(); ++i) {
+            dp[i] = max(dp[i - 1], A[i - 1]) - 1;
+            if (dp[i] < 0) return false;
+        }
+        return true;
+ 
+    }
+};
