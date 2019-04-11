428614b6ae45d873918f56e6aa956ab7a8d80ced
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-11 03:01:00 -0700

    update 664.counting-bits.cpp

--- 664.counting-bits.cpp
+++ 664.counting-bits.cpp
@@ -17,8 +17,8 @@
  *
  * @Category DP
  * @Ideas
- * S1: 从1开始，遇到偶数时，其1的个数和该偶数除以2得到的数字的1的个数相同，遇到奇数时，其1的个数等于该奇数除以2得到的数字的1的个数再加1
- * S2: 
+ * S1: 每个数i的二进制表示中1的个数都是i&(i-1)对应的值加1
+ * S2: 从1开始，遇到偶数时，其1的个数和该偶数除以2得到的数字的1的个数相同，遇到奇数时，其1的个数等于该奇数除以2得到的数字的1的个数再加1
  */
 class Solution {
 public:
@@ -27,6 +27,14 @@ public:
      * @return: an array represent the number of 1's in their binary
      */
     vector<int> countBits(int num) {
+        vector<int> res(num + 1, 0);
+        for (int i = 1; i <= num; ++i) {
+            res[i] = res[i & (i - 1)] + 1;
+        }
+        return res;
+    }
+	
+	vector<int> countBits2(int num) {
         vector<int> res{0};
         for (int i = 1; i <= num; ++i) {
             if (i % 2 == 0) res.push_back(res[i / 2]);
