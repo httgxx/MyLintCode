dac1dbe23c87eafe9ca0113ea7a68d9ab9a9b5c2
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-12 01:22:36 -0700

    update 512.decode-ways.cpp

--- 512.decode-ways.cpp
+++ 512.decode-ways.cpp
@@ -24,11 +24,9 @@
  * 
  * @Category DP
  * @Ideas
- * 特例: 空串解码方法为0
- * 常例限制:一位数时不能为0，两位数不能大于26，其十位上的数也不能为0
- * dp[i]表示到第i个字符有多少种解码方式
- * dp[i]=dp[i-1]|前1位不为0 + dp[i-2]|前两位在10~26即s[i-2]=='1'||(s[i-2]=='2'&&s[i-1]<='6')
- * i从0开始,第0个字符对应的dp[0]=1因为为了使得dp[1]=dp[0]=1,dp数组大小为n+1
+ * dp[i]表示前i个字符有多少种解码方式
+ * dp[i]=dp[i-1]|s[i-1]1位就是某字符的编码 + dp[i-2]|s[i-2]s[i-1]2位一起是某字符的编码
+ * dp[0]=1 但是必须事先单独处理s为空串的情况因为题目要求空串返回0(而不是1)
  */
 class Solution {
 public:
@@ -36,22 +34,22 @@ public:
      * @param s: a string,  encoded message
      * @return: an integer, the number of ways decoding
      */
-    int numDecodings(string &s) {
-        if (s.empty()) { return 0; }
+    int numDecodings(const string &s) {
+        if (s.empty()) { return 0; }  // 按题意特殊处理空串返回0
         vector<int> dp(s.size() + 1, 0);
-        dp[0] = 1; //需要使dp[1]=dp[0]时得到1
-        for (int i = 1; i < dp.size(); ++i) { //第i位数字
-            // 第i位单独解码
-            if (s[i - 1] != '0') { //第i位数字=s[i-1]
-                dp[i] += dp[i - 1]; //坑: += 而不是 =
+        dp[0] = 1;  // 空子串有一种解码方式
+        for (int i = 1; i < dp.size(); ++i) {  // 前i个数字
+            // 前i个数字的最后1位(第i-1个数字)是某字符的编码
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1];  // 坑: += 而不是 =
             }
-            // 第i位与第i-1位一起解码
-            if (i > 1 && //坑: 不要忘记i>1
-                (s[i - 2] == '1' || //坑: i-2而不是i-1
-                 (s[i - 2] == '2' && s[i - 1] <= '6'))) { // 10~26
+            // 前i个数字的最后2位(第i-2和i-1个数字)是某字符的编码
+            if (i >= 2 &&  // 坑: 不要忘记i>1
+                (s[i - 2] == '1' ||  // 坑: i-2而不是i-1
+                 (s[i - 2] == '2' && s[i - 1] <= '6'))) {  // 10~26
                 dp[i] += dp[i - 2];
             }
         }
-        return dp.back(); //不用dp[index]省得index写错
+        return dp.back();  // 不用写dp[s.size()]省得index写错
     }
 };
