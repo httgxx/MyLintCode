972a717db83a615c11b4147ceefec832cb3f0af8
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-12 15:08:51 -0700

    update 114.unique-paths.cpp

--- 114.unique-paths.cpp
+++ 114.unique-paths.cpp
@@ -24,11 +24,12 @@
  * =到上邻居的走法+到做邻居的走法
  * =dp[i-1][j]+dp[i][j-1](到)
  * 初始 dp[0][j]=dp[i][0]=1 //i=0~m-1,j=0~n-1
- * 顺序 上到下i=0->m-1,左到右j=0->n-1 
+ * 顺序 上到下i=0->m-1,左到右j=0->n-1 (也可以左到右,上到下)
  * 返回 dp[m-1][n-1]
  * 
- * S2: T=O(mn) S=O(n)
- * 降维 只用存当前行的每列 
+ * S2: T=O(mn) S=O(min(m,n))
+ * 降维 只存当前行的每列 (或只存当前列的每行)
+ * // 优化 以m和n中小的那个大小来存 大的那个做外循环
  * 新值=旧值/上一行当前列(左上角到上邻居的走法)+当前行前一列(左上角到到左邻居的走法)
  * dp[j]=从左上角到当前行的第j列有多少种走法
  * dp[j]+=dp[j-1] //+=而不是=
@@ -38,8 +39,9 @@
  * S3: T=O(min(m,n)) S=O(1)
  * 总共走m-1+n-1=m+n-2步 要么向下 要么向右 走法就是从m+n-2步中选取min(m,n)-1步的方法
  * 即求组和数C(m+n-2,min(m,n)-1)
- * //排列公式P(n,k)=n*(n-1)*...*(n-k+1)=n!/(n-k)!
- * //组和公式C(n,k)=排列P(n,k)/k!(无序)=n!/(k!*(n-k)!)
+ * // 排列公式P(n,k)=n*(n-1)*...*(n-k+1)=n!/(n-k)!
+ * // 组和公式C(n,k)=排列P(n,k)/k!(无序)=n!/(k!*(n-k)!)
+ * 坑: 要用double求
  * 
  * BF: T=O(?) S=O(stack?)
  * 递归 count(row,col) = count(row-1,col)+cont(row,col-1) 有重复计算
@@ -51,7 +53,7 @@ public:
      * @param n: positive integer (1 <= n <= 100)
      * @return: An integer
      */
-    int uniquePaths1(int m, int n) { //2维
+    int uniquePaths0(int m, int n) { //2维
         vector<vector<int>> dp(m, vector<int>(n, 0));
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
@@ -66,7 +68,9 @@ public:
 		return dp[m - 1][n - 1];
     }
 
-    int uniquePaths(int m, int n) {  // 降维
+    int uniquePaths(int m, int n) {  // 降维 + min优化
+        if (n < m) { return uniquePaths(n, m); }  // 按min的大小存dp值
+
         vector<int> dp(n, 1);  // 第1行初始值:从左上角到第1行的每个格子都只有1种走法
         for (int i = 1; i < m; ++i) {  // 第1行已赋值,从第2行开始
             for (int j = 1; j < n; ++j) {  // 第1列都是1,从第2列开始
@@ -75,4 +79,9 @@ public:
         }
 		return dp[n-1];
     }
+
+    int uniquePaths1(int m, int n) {  // 组合
+        
+    }
+
 };
