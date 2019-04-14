88a88b80f289be08d7a114f39a1ea5b1888bbdee
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-14 11:17:05 -0700

    update 76.longest-increasing-subsequence.cpp

--- 76.longest-increasing-subsequence.cpp
+++ 76.longest-increasing-subsequence.cpp
@@ -19,11 +19,11 @@
  * DP // T=O(n) S=T(n)
  * 最长上升子序列LIS
  * 相同长度的LIS末尾越小越有可能append新的更大数形成更长LIS
- * 记录每种长度的LIS的最小末尾数: ends[i]=所有长度为i的LIS的末尾的最小值 
+ * 记录每种长度的LIS的最小末尾值: ends[i]=所有长度为i的IncresingSequence的末尾值中最小的 
  * 每次访问到新数,用它去替换ends中第1个>=新数的LIS末尾
  * 
- * 注: 若LIS不允许重复数,则ends不允许有重复数; 如果LIS允许重复数,则ends允许有重复数
- *     两种情况下都是用a[i]去替换第一个ends[k]>=a[j] 
+ * 注: 若LIS不允许重复数,则ends不允许有重复数;如果LIS允许重复数,则ends允许有重复数
+ *     两种情况下都是用a[i]去替换第一个>=a[j]的ends[k]
  */
 class Solution {
 public:
@@ -35,20 +35,19 @@ public:
         vector<int> ends; 
         for (int i = 0; i < nums.size(); ++i) {
             int left = 0, right = ends.size();  // ends可能已增长故每次重算right
-            // case1:[i]>当前最长子序列最小末尾,即可append形成更长上升子序列
-            if (right == 0 || nums[i] > ends[right - 1]) {
-               ends.push_back(nums[i]);
+            // case1:[i]>当前LIS最小末尾,即加在最小末尾后形成更长LIS
+            if (right == 0 || nums[i] > ends.back()) {
+               ends.push_back(nums[i]);  // 更新ends
             }
-            // case2:[i]<=当前最长子序列最小末尾,即不能append形成更长上升子序列
-            // 须降低可降低的最长上升子序列的最小末尾中最大的一个            
-            // 即二分找到ends中第一个(最小的)>=[i]的值,并替换成[i]
-            else { //ends中至少还有1个数 即left<=right
-              while (left < right) { //至少还剩2个数
+            // case2:[i]<当前LIS最小末尾,即不能直接形成更长LIS
+            // 即二分找到ends中第一个>=[i]的值来替换成[i]
+            else { // ends中至少还有1个数所以肯定有left<=right
+              while (left < right) {  // 至少还剩2个数
                 int mid = left + (right - left) / 2;
                 if (ends[mid] < nums[i]) {
-                    left = mid + 1;  // [mid]太小 去右边找更大
+                    left = mid + 1;  // [mid]太小,去右边找更大
                 }
-                else {
+                else { // ends[mid] >= nums[i]
                     right = mid;  // [mid]符合条件,去左边(包含mid)找更小
                 }
               }
