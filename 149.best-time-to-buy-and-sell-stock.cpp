fa15f6c7a5e4544cd540f8515f120ea7a472be71
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-20 14:03:26 -0700

    update 149.best-time-to-buy-and-sell-stock.cpp

--- 149.best-time-to-buy-and-sell-stock.cpp
+++ 149.best-time-to-buy-and-sell-stock.cpp
@@ -18,13 +18,13 @@
  * and sell one share of the stock), design an algorithm to find the maximum
  * profit.
  * 
- * @Category DP,枚举
+ * @Category DP(比较型),枚举
  * @Ideas
  * DP // T=O(n) S=O(1)
  * 最多可买卖1次的最大获利
  * = max{第i天卖的获利=第i天价格-第i天之前的最低价}|每天
  * 坑: 先处理特例size<=1
- * 坑: 从第2天(i=1)开始处理即i从1而不是0开始
+ * 坑: i从1而不是0开始,从第2天(i=1)才可以开始卖
  * 坑: 先用旧的最低价算最大收益,再算新的最低价
  */
 class Solution {
@@ -36,10 +36,10 @@ public:
     int maxProfit(const vector<int> &prices) {
         if (prices.size() < 2) { return 0; }  // 只能第1天之后卖
 
-        int profit = 0, buy = prices[0];
-        for (int i = 1; i < prices.size(); ++i) {  // 从第2天开始可以选一天卖了
-            profit = max(profit, prices[i] - buy);  // 当天卖获利=当天-之前最低
-            buy = min(buy, prices[i]);  // 更新目前位置最低价
+        int profit = 0, minPrice = prices[0];  // 当前最低价格
+        for (int i = 1; i < prices.size(); ++i) {  // 坑:i=1,第2天才可以开始卖
+            profit = max(profit, prices[i] - minPrice); // 最大利=当天-之前最低
+            minPrice = min(minPrice, prices[i]);  // 时刻保持当前最低价格
         }
         return profit;
     }
