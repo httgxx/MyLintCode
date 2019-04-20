9caf366d82aa8d785c82acc158be2ac91166b3ff
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-20 16:50:30 -0700

    update 76.longest-increasing-subsequence.cpp

--- 76.longest-increasing-subsequence.cpp
+++ 76.longest-increasing-subsequence.cpp
@@ -14,16 +14,23 @@
  * Given a sequence of integers, find the longest increasing subsequence (LIS).
  * You code should return the length of the LIS.
  * 
- * @Category DP
+ * @Category DP(按情况叠加比较型)
  * @Ideas
- * DP // T=O(n) S=T(n)
+ * S1: // T=O(nlogn) S=O(n) 
  * 最长上升子序列LIS
  * 相同长度的LIS末尾越小越有可能append新的更大数形成更长LIS
  * 记录每种长度的LIS的最小末尾值: ends[i]=所有长度为i的IncresingSequence的末尾值中最小的 
- * 每次访问到新数,用它去替换ends中第1个>=新数的LIS末尾
- * 
+ * 每次访问到新数,用它去替换ends中第1个>=新数的LIS末尾 
  * 注: 若LIS不允许重复数,则ends不允许有重复数;如果LIS允许重复数,则ends允许有重复数
  *     两种情况下都是用a[i]去替换第一个>=a[j]的ends[k]
+ * 
+ * S2: // T=O(n^2) S=O(n)
+ * dp[i]表示以a[i]结尾的LIS长度
+ * =以a[i]前面的某个<a[i]的a[k]为结尾的LIS中最长的长度+1
+ * =max{dp[k]|k<i且a[k]<a[i]} + 1
+ * 初始 dp[0]=1
+ * 顺序 i=0~n-1, k=i~i-1
+ * 坑:
  */
 class Solution {
 public:
@@ -31,23 +38,21 @@ public:
      * @param nums: An integer array
      * @return: The length of LIS (longest increasing subsequence)
      */
-    int longestIncreasingSubsequence(vector<int> &nums) {
-        vector<int> ends; 
+    int longestIncreasingSubsequence1(const vector<int> &nums) {
+        vector<int> ends;
         for (int i = 0; i < nums.size(); ++i) {
             int left = 0, right = ends.size();  // ends可能已增长故每次重算right
-            // case1:[i]>当前LIS最小末尾,即加在最小末尾后形成更长LIS
             if (right == 0 || nums[i] > ends.back()) {
+               // case1:[i]>当前LIS最小末尾,即加在最小末尾后形成更长LIS
                ends.push_back(nums[i]);  // 更新ends
-            }
-            // case2:[i]<当前LIS最小末尾,即不能直接形成更长LIS
-            // 即二分找到ends中第一个>=[i]的值来替换成[i]
-            else { // ends中至少还有1个数所以肯定有left<=right
+            } else {  // ends中至少还有1个数所以肯定有left<=right
+              // case2:[i]<当前LIS最小末尾,即不能直接形成更长LIS
+              // 即二分找到ends中第一个>=[i]的值来替换成[i]
               while (left < right) {  // 至少还剩2个数
                 int mid = left + (right - left) / 2;
                 if (ends[mid] < nums[i]) {
                     left = mid + 1;  // [mid]太小,去右边找更大
-                }
-                else { // ends[mid] >= nums[i]
+                } else {  // ends[mid] >= nums[i]
                     right = mid;  // [mid]符合条件,去左边(包含mid)找更小
                 }
               }
@@ -55,5 +60,21 @@ public:
             }
         }
         return ends.size();
+    }
+
+    int longestIncreasingSubsequence(const vector<int> &nums) {
+        int n = nums.size();
+        if (n < 2) { return n; }
+        int res = 0;
+        vector<int> dp(n, 1);  // 坑:初始值为1而不是0!
+        for (int i = 1; i < n; ++i) {  // 以[i]结尾
+            for (int k = 0; k < i; ++k) {  // 找[i]前面[k]<[i]
+                if (nums[k] < nums[i]) {
+                    dp[i] = max(dp[i], dp[k] + 1);
+                }
+            }
+            res = max(res, dp[i]);  // 坑:最后不是返回dp[n-1]而是max(dp[i])
+        }
+        return res;
     }
 };
