1770c84cb8817c918719edd827b20bd8c2e9e820
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-21 01:23:08 -0700

    update 602.russian-doll-envelopes.cpp

--- 602.russian-doll-envelopes.cpp
+++ 602.russian-doll-envelopes.cpp
@@ -17,34 +17,43 @@
  * other envelope.
  * Find the maximum number of nested layers of envelopes.
  * 
- * @Category DP,贪心?
+ * @Category DP,LIS
  * @Ideas
- * dp[i]表示第i个信封能套的最高的信封高度
+ * DP //T=O(nlogn) S=O(n)
+ * 按照长度从小到大排O(nlog),注意相同长度则按宽度从大到小排(因为同长时小宽仍放不进大宽)
+ * 找宽度的LIS的长度
+ * dp[i]
  * 
  */
 class Solution {
 public:
+    static bool cmp(const pair<int, int> &x, const pair<int, int> &y) {
+        // 按宽度从小到大排,同宽时按高度从大到小排,因为同宽时矮的仍然放不进高的
+        return x.first != y.first ? x.first < y.first : x.second > y.second;
+    }
     /*
      * @param envelopes: a number of envelopes with widths and heights
      * @return: the maximum number of envelopes
      */
     int maxEnvelopes(vector<pair<int, int>>& envelopes) {
-        vector<int> dp;
-        sort(envelopes.begin(), envelopes.end(), 
-            [](const pair<int, int> &a, const pair<int, int> &b) {
-                // 同样宽度则按高度从大到小排 这样才能保证同宽的就算高度高也不能套
-                if (a.first == b.first) return a.second > b.second; 
-                return a.first < b.first;  // 先按宽度从小到大排
-        });
-        for (int i = 0; i < envelopes.size(); ++i) {
-            int left = 0, right = dp.size(), height = envelopes[i].second;
-            while (left < right) {  // 二分找到第一个高度>=信封[i]的信封
+        int n = envelopes.size();
+        if (n < 2) { return n; }
+
+        sort(envelopes.begin(), envelopes.end(), cmp);  // 按宽度升序排的信封
+        vector<int> dp;  // 坑:不能初始化时设dp[]的大小,因为长度会不断增大
+        for (int i = 0; i < n; ++i) {  // 找基于高度的LIS
+            int left = 0, right = dp.size(), h = envelopes[i].second;
+            while (left < right) {  // 二分找到第1个高度>=height的信封
                 int mid = left + (right - left) / 2;
-                if (dp[mid] < height) left = mid + 1;
-                else right = mid;
+                if (dp[mid] < h) { left = mid + 1; }
+                else { right = mid; }  // dp[mid]>=h但还继续往左找最左边>=h的数
             }
-            if (right >= dp.size()) dp.push_back(height);  // 找不到则高度入栈
-            else dp[right] = height;  // 找到了则用更新能套的信封的最高高度
+            if (right >= dp.size()) {
+                dp.push_back(h);  // 没找到即height是目前最高的,增长LIS
+            }
+            else {
+                dp[right] = h;  // 找到了这样的高度,用height替换降低它
+            }
         }
         return dp.size();
     }
