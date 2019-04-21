be126b0070164523e481027d34271c1ed9e79363
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-20 17:31:30 -0700

    update 397.longest-continuous-increasing-subsequence.cpp

--- 397.longest-continuous-increasing-subsequence.cpp
+++ 397.longest-continuous-increasing-subsequence.cpp
@@ -17,11 +17,14 @@
  * - Can be from right to left or from left to right.
  * - Indices of the integers in the subsequence should be continuous.
  * 
- * @Category 
+ * @Category DP(按情况叠加比较型),单调性
  * @Ideas
- * 单调性 //T=O(n) S=O(1)
+ * DP //T=O(n) S=O(1)
  * 最长单调递增/单调递减子串: 从左到右两两比较,每次单调性变化时候更新最长单调子串长度
+ * 如果继续单调则对应单调子串长度+1,如果不在继续单调则对应单调子串长度=1
  * 坑: 题目说左到右或者右到左单调递增 故也要考虑单调递减子串长度
+ * 坑: 得先处理size<2不够形成单调子串的特例
+ * 坑: i从1而不是0开始循环处理
  */
 class Solution {
 public:
@@ -31,15 +34,15 @@ public:
      */
     int longestIncreasingContinuousSubsequence(vector<int> &A) {
         int n = A.size();
-        if (n <= 1) return n;  // 特例:不够长
+        if (n < 2) return n;  // 特例:不够长
         
-        int incrLen = 1, descLen = 1, maxLen = 1;
-        for (int i = 1; i < n; ++i) {
-            if (A[i] > A[i - 1]) { ++incrLen; }  // 发现单调递增
-            else { incrLen = 1; }  // 不再单调递增
+        int incrLen = 1, descLen = 1, maxLen = 1;  // 递增子串长度,递减子串长度
+        for (int i = 1; i < n; ++i) {  // i从1开始
+            if (A[i] > A[i - 1]) { ++incrLen; }  // 发现单调递增,递增子串长+1
+            else { incrLen = 1; }  // 不再单调递增,重置递增子串长为1
             
-            if (A[i] < A[i - 1]) { ++descLen; }  // 发现单调递减
-            else { descLen = 1; }  // 不再单调递减
+            if (A[i] < A[i - 1]) { ++descLen; }  // 发现单调递减,递减子串长+1
+            else { descLen = 1; }  // 不再单调递减,重置递减子串长为1
             
             maxLen = max(maxLen, max(incrLen, descLen));
         }
