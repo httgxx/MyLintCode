73e16bb018d79c869d63339b126f2d7e7d813d0c
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-28 18:48:49 -0700

    update 108.palindrome-partitioning-ii.cpp

---
+++ 108.palindrome-partitioning-ii.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=lintcode id=108 lang=cpp
+ *
+ * [108] Palindrome Partitioning II
+ *
+ * https://www.lintcode.com/problem/palindrome-partitioning-ii/description
+ *
+ * lintcode
+ * Medium (24.00%)
+ * Total Accepted:    14455
+ * Total Submissions: 59332
+ * Testcase Example:  '"a"\n'
+ *
+ * Given a string `s`, cut `s` into some substrings such that every substring
+ * is a palindrome. 
+ * 
+ * Return the **minimum** cuts needed for a palindrome partitioning of *s*.
+ * 
+ * Example 1:
+ *   Input: "a"
+ *   Output: 0
+ *   Explanation: "a" is already a palindrome, no need to split.
+ * Example 2:
+ *   Input: "aab"
+ *   Output: 1
+ *   Explanation: Split "aab" once, into "aa" and "b", both palindrome.
+ * 
+ * @Category DP
+ * @Idea
+ * DP T=O(n^2) S=O(n^2)
+ * dp[i]表示前i个字符s[0~i-1]最少被划分为多少个回文串/区间
+ * dp[i]=min(dp[i],dp[j]+1|0<=j<=i-1且s[j~i-1]是回文串)
+ * 初始 dp[0]=0
+ * 坑 j=0~i-1
+ * 坑 求划分次数而不是划分区间数,即返回dp[n]-1
+ * 坑 :中心发散匹配两边 T=O(n^2) S=O(1))
+ 
+ * 判断s[l~r]是否是回文串, T=O(n^2) S=O(n^2)
+ * case1: l==r     x => s[l]==s[r] && r-l==0
+ * case2: r=l+1   xx => s[l]==s[r] && r-l==1
+ * case3: r>l+1 x..x => s[l]==s[r] && r-l>1 && isPal[l+1][r-1]
+ *       合并判断条件 => s[i]==s[j] && (r-l<=1 || isPal[l+1][r-1])
+ * 注: r-l>1时[r]和[l]之间还有字符,所以l+1和r-1都不会坐标越界
+ */
+class Solution {
+public:
+    /**
+     * @param s: A string
+     * @return: An integer
+     */
+    int minCut1(string &s) {
+        if (s.empty()) return 0;  // 特例
+        int n = s.size();
+        vector<vector<bool>> isPalin(n, vector<bool>(n));
+        vector<int> dp(n);  // dp[i]=s[0~i]最少被分为几个回文串
+        for (int i = 0; i < n; ++i) {  // 尾s[i]
+            dp[i] = i + 1;  // 最多每个字符自为回文串共i+1个
+            for (int j = 0; j <= i; ++j) {  // 头s[j]
+                // 坑: 判断s[j~i]是否回文串
+                if (s[i] == s[j] && (i - j < 1 || isPalin[j + 1][i - 1])){
+                    // 注: i-j>1时s[j]和s[i]之间还有字符则i-1和j+1都不会越界
+                    isPalin[i][j] == true;
+                }
+                dp[i] = (j == 0) ? 0 : min(dp[i], dp[j - 1] + 1);
+            }
+        }
+        return dp[n - 1] - 1;  // 坑：求划分次数为不是划分区间数
+    }
+
+    int minCut(string &s) {
+        if (s.empty()) return 0;  // 特例
+        int n = s.size();
+        vector<vector<bool>> p(n, vector<bool>(n, false));  //存各子串是否回文
+        vector<int> dp(n);  // dp[i]=[0~i]最少切几刀成回文串 //坑:几刀而非几段!
+        for (int r = 0; r < n; ++r) {  // 尾 right
+            dp[r] = r;  // 最多每个字符自成回文串,r为index则有r+1段,但只用切r刀
+            for (int l = 0; l <= r; ++l) {  // 头 left
+                if (s[l] == s[r] && (r - l < 2 || p[l + 1][r - 1])) {
+                    p[l][r] = true;
+                    dp[r] = (l == 0) ? 0 : min(dp[r], dp[l - 1] + 1);
+                    // 坑: l为0时整个子串都回文不需切,但dp须赋0否则仍是初始值r 
+                }
+            }
+        }
+        return dp[n - 1];  // 坑：dp存几刀而不是存几段!
+    }
+};
