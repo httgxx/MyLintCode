4950330726aa797427c6240b47c22ab0774e8851
httgxx <42990730+httgxx@users.noreply.github.com>
2019-04-28 18:19:08 -0700

    update 393.best-time-to-buy-and-sell-stock-iv.cpp

--- 393.best-time-to-buy-and-sell-stock-iv.cpp
+++ 393.best-time-to-buy-and-sell-stock-iv.cpp
@@ -25,6 +25,8 @@
  * local[i][j] = max(global[i-1][j-1], local[i-1][j] + prices[i]-prices[i-1])
  * global[i][j] = max(local[i][j], global[i-1][j])
  * 坑: 千万别忘记case1 k>=n/2的情况!
+ * 坑: 若降维则需要倒着j计算,因为计算local[i][j]要用到global[i-1][j-1],需用旧[j-1]算新[j],
+ *     而降维后只存当前i的各个j值,所以[j-1]的更新只能在[j]的更新之后,否则是在用新[j-1]算新[j]
  * 
  * 公式推导
  * global[i][j]为第i天时最多j次交易的最大利润,是全局最优
@@ -33,11 +35,11 @@
  * 2. 第i天不卖: 第i天没交易,等价于第i-1天最多j次交易:global[i][j] = global[i-1][j]
  * =>递推公式 global[i][j] = max(global[i-1][j], local[i][j])
  * 
- * local[i][j] 为第i天时最多j次交易且第i天卖第j支股票的最大利润,是局部最优
+ * local[i][j]为第i天时最多j次交易且第i天卖第j支股票的最大利润,是局部最优
  * 第i天卖的第j支股票利润多少=>何时买的?
  * 1.第i-1天买的:
  *   local[i][j]=global[i-1][j-1]+(prices[i]-prices[j])
- * 2.第i-1天之前买的:不好计算先前哪天买的以及买入价,就用差值法讲买入价抵消
+ * 2.第i-1天之前买的:不好计算先前哪天买的以及买入价,就用差值法将买入价抵消
  *   第i天卖的利润 = 若第i-1天卖的利润 + (第i天卖的利润-若第i-1天卖的利润)
  *   local[i][j]=local[i-1][j]+(prices[i]-buy)-(prices[i-1]-buy)
  *              =local[i-1][j]+(prices[i]-prices[i-1])
@@ -53,12 +55,12 @@ public:
     int maxProfit(int k, vector<int> &prices) {
         if (prices.empty()) return 0;
         if (k >= prices.size()/2) return solveMaxProfit(prices);
-        int g[k + 1] = {0};
+        int g[k + 1] = {0};  // 坑 降维后
         int l[k + 1] = {0};
         for (int i = 0; i < prices.size() - 1; ++i) {
             int diff = prices[i + 1] - prices[i];
-            for (int j = k; j >= 1; --j) {
-                l[j] = max(g[j - 1] + max(diff, 0), l[j] + diff);
+            for (int j = k; j >= 1; --j) {  // 坑:降维后须倒着j计算
+                l[j] = max(g[j - 1], l[j] + diff);
                 g[j] = max(g[j], l[j]);
             }
         }
