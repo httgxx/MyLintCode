cce9385a68feae83d3470164e9a72aec80442a9b
httgxx <42990730+httgxx@users.noreply.github.com>
2019-05-05 15:58:26 -0700

    update 92.backpack.cpp

--- 92.backpack.cpp
+++ 92.backpack.cpp
@@ -27,21 +27,51 @@
  * 
  * @Category DP(01背包型)
  * @Idea
- * S1: dp[i]表示背包容量为i时能放入的物品体积最大值
- * dp[i]= 
- * for i=0~n  // 每个物品
-     for j=m~1  // 倒循环每种背包容量
-       if(j>=a[i])  // 若背包容量>=物品i体积a[i]
-         dp[j]=max(dp[j], dp[j-a[i]]+a[i])  // max(不放a[i],放a[i])
- * 初始 dp[0]=0 dp.size=n+1
- * 返回 dp[n]
+ * S1: DP // T=O(mn) S=O(m)
+ * dp[i]表示背包容量为i时能放入的物品体积最大值
+ * dp[i]= 每多1个物品可选放或不放时,背包容量由小到大时能放入的物品体积和最大值
+ * for j=0~n  // 每个物品
+ *   for i=m~a[j]  // 每种能装下当前物品的背包容量
+ *      dp[i]=max(dp[i], dp[i-a[j]]+a[j])  // max(不放a[i],放a[i])
+ * 初始 dp[0~m]=0
+ * 顺序 外循环第0~n-1个物品,内循环背包容量m~A[i](必须倒叙)因为需根据旧值而不是新值修改当前值
+ * 返回 dp[m]
+ * 坑 初始化成0而不是INT_MIN
  * 坑 dp[i]的i是背包容量大小i,不是第i个物品的i
+ * 坑 必须倒循环i=m~A[j]因为要用旧dp[i-A[j]]计算
  * 
- * S2: dp[i]表示体积和V能否被构成(T/F))
- * dp[i]= 物品i放或不放
- * 返回 最大的i使dp[i]=true
+ * 实例 [3,4,8,5] m=9
+ * j=0 a[j]=3
+ *   i=9 dp[9]=max(0,dp[9-3]+3=3)=3
+ *   i=8 dp[8]=max(0,dp[8-3]+3=3)=3
+ *   i=7 dp[7]=max(0,dp[7-3]+3=3)=3
+ *   i=6 dp[6]=max(0,dp[6-3]+3=3)=3
+ *   i=5 dp[5]=max(0,dp[5-3]+3=3)=3
+ *   i=4 dp[4]=max(0,dp[4-3]+3=3)=3
+ *   i=3 dp[3]=max(0,dp[3-3]+3=3)=3
+ * j=1 a[j]=4
+ *   i=9 dp[9]=max(3,dp[9-4]+4=3+4=7)=7  //坑:若不倒循环i=m->A[j]则用到的dp[5]是新值4而不是旧值3
+ *   i=8 dp[8]=max(3,dp[8-4]+4=3+4=7)=7
+ *   i=7 dp[7]=max(3,dp[7-4]+4=3+4=7)=7
+ *   i=6 dp[6]=max(3,dp[6-4]+4=0+4=4)=4
+ *   i=5 dp[5]=max(3,dp[5-4]+4=0+4=4)=4
+ *   i=4 dp[4]=max(3,dp[4-4]+4=0+4=4)=4
+ * j=2 a[j]=8
+ *   i=9 dp[9]=max(7,dp[9-8]+8=0+8=8)=8
+ *   i=8 dp[8]=max(7,dp[8-8]+8=0+8=8)=8
+ * j=3 a[j]=5
+ *   i=9 dp[9]=max(8,dp[9-5]+5=4+5=9)=9
+ *   i=8 dp[8]=max(8,dp[8-5]+5=4+5=9)=9
+ *   i=7 dp[7]=max(7,dp[7-5]+5=0+5=5)=7
+ *   i=6 dp[6]=max(4,dp[6-5]+5=0+5=5)=5
+ *   i=5 dp[5]=max(4,dp[5-5]+5=0+5=5)=5
+ * return dp[9]=9
  * 
- * 参考 https://segmentfault.com/a/1190000006325321
+ * S2: DP // T=O(mn) S=O(mn)
+ * dp[i][j]表示前i个物品能否被构成体积和j(true/false)
+ * dp[i][j]=不用当前最后1个物品[i-1](前i-1个物品已经构成j) or 用[i-1](前i-1个物品构成j-a[i-1])
+ * = dp[i-1][j] || dp[i-1][j-A[i-1]]|j>A[i-1]
+ * 返回 使dp[n][j]=true的最大j
  */
 class Solution {
 public:
@@ -51,12 +81,31 @@ public:
      * @return: The maximum size
      */
     int backPack(int m, vector<int> &A) {
-        vector<int> dp(m + 1, 0);
-        for (int j = 0; j < A.size(); ++j) {
-            for (int i = m; i >= A[j]; --i) {
+        vector<int> dp(m + 1, 0);  // 坑: 初始化成0而不是INT_MIN
+        for (int j = 0; j < A.size(); ++j) {  // 每个物品
+            for (int i = m; i >= A[j]; --i) {  // 坑: 必须倒循环因为用的旧dp[i - A[j]
                 dp[i] = max(dp[i], dp[i - A[j]] + A[j]);
             }
         }
         return dp[m];
+    }
+
+    int backPack1(int m, vector<int> &A) {
+        int n = A.size();
+        if (n == 0) return 0;
+
+        vector<vector<bool>> dp(n, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= m; ++j) {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - A[i - 1]];
+            }
+        }
+
+        for (int j = m; j >=0; --j) {  // 找到使得dp[n][j]为true的最大j
+            if (dp[n][j]) return j;
+        }
+
+        return 0;
     }
 };
