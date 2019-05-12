79c238815df29f66663b125bbaecf92e389f50da
httgxx <42990730+httgxx@users.noreply.github.com>
2019-05-11 17:52:46 -0700

    update 440.backpack-iii.cpp

--- 440.backpack-iii.cpp
+++ 440.backpack-iii.cpp
@@ -26,17 +26,11 @@ public:
      * @return: an array
      */
     int backPackIII(vector<int> &A, vector<int> &V, int m) {
-        /*vector<int> f(m + 1, 0);  // 初始化为0 坑:f(m+1,0) 不是f(n+1,0)!!
-        
+        vector<int> f(m + 1, 0);  // 初始化为0 坑:f(m+1,0) 不是f(n+1,0)!!
         for (int i = 0; i < A.size(); ++i) {  // [i] 坑: 体积存在A[i]而不是V[i]!!!
             for (int j = A[i]; j <= m; ++j) {  // 须倒算!! 因为大index的新值要用小index的新值来算
-                f[j] = max(f[j], f[i - A[i]] + V[i]);  // 只max用[j]时的价值,因不用[j]时价值在考虑[j]前后不变 
-            }
-        }*/
-        vector<int> f(m + 1, 0);
-        for (int i = 0; i < A.size(); ++i) {
-            for (int j = A[i]; j <= m; ++j) {
-                f[j] = max(f[j], f[j - A[i]] + V[i]);
+                f[j] = max(f[j], f[j - A[i]] + V[i]);  // 只max用[j]时的价值,因不用[j]时价值在考虑[j]前后不变
+                                                       // 坑: f[j-A[i]] 不是 f[i-A[i]]!!! 
             }
         }
         return f[m];
