8705095181cc8864fa43d35a822d4517e8d18fa0
httgxx <42990730+httgxx@users.noreply.github.com>
2019-08-29 00:08:37 -0700

    update 68.binary-tree-postorder-traversal.cpp

--- 68.binary-tree-postorder-traversal.cpp
+++ 68.binary-tree-postorder-traversal.cpp
@@ -38,10 +38,15 @@ public:
      * @param root: A Tree
      * @return: Postorder in ArrayList which contains node values.
      */
+
+    // S1: 
     vector<int> postorderTraversal1(TreeNode * root) {
         // write your code here
     }
-    vector<int> postorderTraversal(TreeNode* root) {
+
+    // S2: root-right-left => reverse => left-right-root
+    // T=O(n) S=O(n)
+    vector<int> postorderTraversal2(TreeNode* root) {
         if (!root) return {};  // corner case: null tree
         vector<int> res;
         stack<TreeNode*> s{{root}};  // root in stack
@@ -50,6 +55,25 @@ public:
             res.insert(res.begin(), t->val);  // (1,2,3):(root-right-left)=>reverse=>(left, right, root)
             if (t->left) s.push(t->left);     // (2,3):in stack(left,right)=>out stack(right, left)
             if (t->right) s.push(t->right);   
+        }
+        return res;
+    }
+
+    // S3: root-right-left => reverse => left-right-root
+    // T=O(n) S=O(n)
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode *p = root;
+        while (!s.empty() || p) {
+            if (p) {
+                s.push(p);                        // 1:root
+                res.insert(res.begin(), p->val);  // (1,2,3):(root-right-left)=>reverse=>(left, right, root)
+                p = p->right;                     // 2:right
+            } else {
+                TreeNode *t = s.top(); s.pop();   // 3:left
+                p = t->left;
+            }
         }
         return res;
     }
