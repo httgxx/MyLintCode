5ca65dfee90ca75d1d5744606aca11942cad138f
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-01 00:28:36 -0700

    update 95.validate-binary-search-tree.cpp

--- 95.validate-binary-search-tree.cpp
+++ 95.validate-binary-search-tree.cpp
@@ -78,7 +78,9 @@ public:
     }
     bool helper(TreeNode * root, long mn, long mx) {
         if (!root) return true;
-        if (root->val <= mn || root->val >= mx) return false;
-        return helper(root->left, mn, root->val) && helper(root->right, root->val, mx);
+        return root->val > mn && root->val < mx &&
+            helper(root->left, mn, root->val) && helper(root->right, root->val, mx);
     }
+
+    // S2:
 };
