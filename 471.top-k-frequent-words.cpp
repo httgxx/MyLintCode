b6e7dd5d8e59e69011c63a38e0e3aab5ab5c4696
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-02 20:18:44 -0700

    update 471.top-k-frequent-words.cpp

--- 471.top-k-frequent-words.cpp
+++ 471.top-k-frequent-words.cpp
@@ -29,7 +29,7 @@ public:
      * @return: an array of string
      */
     // S1: map + maxHeap // T=O(nlogk) S=O(n)
-    vector<string> topKFrequentWords(vector<string> &words, int k) {
+    vector<string> topKFrequentWords1(vector<string> &words, int k) {
         vector<string> res(k);  // !!!先初始化大小之后才能反向写入
         unordered_map<string, int> freq;
         auto cmp = [](pair<string, int>& a, pair<string, int>& b) {  // 自定义比较器:freq降序,word升序
@@ -43,6 +43,27 @@ public:
         }
         for (int i = res.size() - 1; i >= 0; --i) {  // 用的是最小堆,所以是从小到达输出,需要从数组尾往前赋值
             res[i] = q.top().first; q.pop();
+        }
+        return res;
+    }
+
+    // S2: map + 桶Bucket<set> 
+    vector<string> topKFrequentWords(vector<string> &words, int k) {
+        vector<string> res;
+        unordered_map<string, int> freq;
+        vector<set<string>> buckets(words.size() + 1, set<string>());  // size+1 set自动按字母升序排序
+        
+        for (string word : words) { ++freq[word]; } // 统计频率
+        for (auto a : freq) { buckets[a.second].insert(a.first); }  //入桶:按频率找桶,set.insert升序
+        for (int i = buckets.size() - 1; i >= 0; --i) { // 出桶: 倒着访问桶
+            if (k <= 0) break;
+            vector<string> t(buckets[i].begin(), buckets[i].end());  // set转换成vector才能index访问
+            if (k >= t.size()) {
+                res.insert(res.end(), t.begin(), t.end());  // 此桶全出 加到res尾
+            } else {
+                res.insert(res.end(), t.begin(), t.begin() + k); // 此桶部分出 加到res尾
+            }
+            k -= t.size(); // 更新剩下需要输出的数目
         }
         return res;
     }
