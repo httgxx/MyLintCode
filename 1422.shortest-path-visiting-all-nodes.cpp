9a29d5fba708bf39db77ce7445d638f0bedc7d42
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-04 04:03:29 -0700

    update 1422.shortest-path-visiting-all-nodes.cpp

--- 1422.shortest-path-visiting-all-nodes.cpp
+++ 1422.shortest-path-visiting-all-nodes.cpp
@@ -45,12 +45,14 @@
  * One possible path is [0,1,4,2,3]
  *
  * @Category Graph, Shortest Path
- * @Idea 求最短路径=>BFS T=O(n*2^n) S=O(n*2^n)
+ * @Idea 求最短路径 BFS
+ * // T=O(n*2^n) S=O(n*2^n)
  * 难点1:如何避重? 既要允许重复访问一个顶点,又要避免死循环?
  *  思路:什么情况下会死循环?再次访问同一个顶点时发现被访问过的顶点集并没有发生变化
  *      即上次访问该顶点时有哪些顶点被访问过,这次还是这些顶点被访问过,故避重条件
  *      要从"访问顶点i时,顶点i没被访问过(访问状态不同)"state=bool cur_node_status //visited or not 
  *      变成"访问顶点i时,顶点集合的访问状态都不同"state=(int cur_node_idx, set<int> all_visited_nodes)
+ *      state最多可能n*2^n种 => BFS最多访问n*2^n次 => T=S=O(n*2^n)
  * 优化1:bit map 
  *      set<int> => int //每个bit记录某顶点是(1)否(0)被访问过
  *      state=(int cur_node_idx, int all_nodes_status)
@@ -60,6 +62,9 @@
  * 难点2:如何做到从任意点出发,即每顶点都可作起点?
  *       开始所有顶点都入队 e.g. q{<0,001>,<1,010>,<2,100>}
  *       每次访问完一个顶点就更新all_node_status 从node0出发访问node1后<0,001>=><1,101>
+ * 
+ * 坑: 处理特例graph=[[]] 
+ * 坑:
  */
 class Solution {
 public:
@@ -73,10 +78,10 @@ public:
         if (graph[0].empty()) { return 0; } // 坑!!!: 特例 [[]]
 
         int steps = 0;
-        int final = (1 << n) - 1;      // 目标状态: 所有n个顶点都被访问过
-        unordered_set<int> visited;    // 已访问过的状态(编码)
-        queue<pair<int, int>> q;       // <当前结点,当前状态>
-        for (int i = 0; i < n; ++i) {  // 初始化: 所有顶点入队,即可从任意顶点出发
+        int final = (1 << n) - 1;      // 目标状态=n位1:所有n个顶点都被访问过
+        unordered_set<int> visited;    // 已访问的状态(编码)
+        queue<pair<int, int>> q;       // <当前顶点,当前所有顶点访问状态>
+        for (int i = 0; i < n; ++i) {  // 初始化:所有顶点入队,即从每个顶点出发
             q.push({i, 1 << i});        
         }
         
@@ -85,11 +90,11 @@ public:
             while (cnt--) {
                 auto p = q.front(); q.pop();
                 int node = p.first, state = p.second;
-                if (state == final) { return steps; }       // 结束否
-                int stateCode = (node << 16) | state;       // 状态编码
+                if (state == final) { return steps; }       // 分层遍历,先结束的最短
+                int stateCode = (node << 16) | state;       // 状态编码,最多n*2^n种=>O(n*2^n)
                 if (visited.count(stateCode)) { continue; } // 避重
                 visited.insert(stateCode);                  // 存状态以避重
-                for (int neighbor : graph[node]) {          // 更新状态 邻居入队
+                for (int neighbor : graph[node]) {          // 更新状态,邻居入队
                     q.push({neighbor, state | (1 << neighbor)});
                 }
             }
