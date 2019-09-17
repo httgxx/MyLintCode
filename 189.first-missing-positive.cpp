177a4a99621efe69a99af46857ab334f81e85da0
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-17 00:02:56 -0700

    update 189.first-missing-positive.cpp

---
+++ 189.first-missing-positive.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=lintcode id=189 lang=cpp
+ *
+ * [189] First Missing Positive
+ *
+ * https://www.lintcode.com/problem/first-missing-positive/description
+ *
+ * lintcode
+ * Medium (24.00%)
+ * Total Accepted:    13015
+ * Total Submissions: 53223
+ * Testcase Example:  '[]\n'
+ *
+ * Given an **unsorted** integer array, find the first **missing** positive
+ * integer.
+ * 
+ * Example 1:
+ * Input: [1,2,0]
+ * Output: 3
+ * 
+ * Example 2:
+ * Input: [3,4,-1,1]
+ * Output: 2
+ * 
+ * Example 3:
+ * Input: [7,8,9,11,12]
+ * Output: 1
+ * 
+ * Note: Your algorithm should run in O(n) time and uses constant extra space.
+ * 
+ * S1: 扫描数组看是否A[i]=i+1,如A[0]=1,A[1]=2,否则如A[i]=j+1则将A[i]和A[j]交换使A[j]=j+1
+ *     交换后若任然有A[i]!=i+1,则继续将A[i]=x和A[x]交换直到A[i]=i+1, 或
+ *     然后再从头扫描一次数组看哪一个位置i上不是i+1,则i就是最小缺的正数     // T=O(n) S=O(1)
+ * S2: Set存所有数字并找出max,从1到max找最小缺的正数,1到max都有则返回max+1 // T=O(n) S=O(n)
+ * 
+ */
+class Solution {
+public:
+    /**
+     * @param A: An array of integers
+     * @return: An integer
+     */
+    // S1: 从左扫到右,若A[i]=i+1继续,否则交换A[i]=j和A[j-1]使得A[j-1]=j,再看
+    //int firstMissingPositive(vector<int> &A) {
+        
+    //}
+
+
+    // S2: Set存所有数字并找出max,从1到max找最小缺的正数,1到max都有则返回max+1 // T=O(n) S=O(n)
+    int firstMissingPositive(vector<int>& nums) {
+        int mx = 0;
+        unordered_set<int> s;
+        for (int num : nums) {
+            if (num <= 0) continue;
+            s.insert(num);
+            mx = max(mx, num);
+        }
+        for (int i = 1; i <= mx; ++i) {
+            if (!s.count(i)) return i;
+        }
+        return mx + 1;
+    }
+
+};
