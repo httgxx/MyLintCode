d785f638b0a9b8cbad26ca75fa39274a5d32e0a7
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-18 22:45:08 -0700

    update 386.longest-substring-with-at-most-k-distinct-characters.cpp

--- 386.longest-substring-with-at-most-k-distinct-characters.cpp
+++ 386.longest-substring-with-at-most-k-distinct-characters.cpp
@@ -27,7 +27,7 @@
  * 
  * @Category HashTable + Sliding Window / two pointers
  * @Idea
- * S1: hashtable记字母频率 + 右进1(左进n)
+ * S1: hashtable记字母频率 + 滑动窗口保证最多K个不同字符
  * 
  * 
  */
@@ -38,17 +38,26 @@ public:
      * @param k: An integer
      * @return: An integer
      */
+    // S1: hashtable记字母频率 + 滑动窗口保证最多K个不同字符
+    // 窗口内满足条件hashtable.size()<=2, 先右边界+1再左边界
+    // T= O(n) S=O(1)
     int lengthOfLongestSubstringKDistinct(string &s, int k) {
         int res = 0, left = 0;                                  // 坑: ,而不是;
         unordered_map<char, int> m;
         for (int right = 0; right < s.size(); ++right) {        // 右边界+1扩大窗口
-            ++m[s[right]];                                          // 频率+1, 若m不含则新建s[i]
-            while (m.size() > k) {                              // 左边界不断+1缩小窗口直到窗口内满足条件
+            ++m[s[right]];                                      // 频率+1, 若m不含则新建s[i]
+            while (m.size() > k) {                              // 左边界不断+1缩小窗口直到满足条件
                 if (--m[s[left]] == 0) { m.erase(s[left]); }    // 频率-1, 若频率0则删除s[i]
                 ++left;
             }
-            res = max(res, right - left + 1);                   // 更新最大窗口大小 // 坑: 勿忘right-left+1
+            res = max(res, right - left + 1);                   // 更新最大窗口大小 // 坑:勿忘+1!!!
         }
         return res;
     }
+
+    // S2: hashtable记字母最后出现位置 + 滑动窗口保证最多K个不同字符
+    // T= O(n) S=O(1)
+    //int lengthOfLongestSubstringKDistinct(string &s, int k) {
+    
+    //}
 };
