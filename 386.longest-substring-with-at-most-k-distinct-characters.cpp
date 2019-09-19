c2601736119939a6e37d7900399bd6edd02f1547
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-18 22:34:58 -0700

    update 386.longest-substring-with-at-most-k-distinct-characters.cpp

---
+++ 386.longest-substring-with-at-most-k-distinct-characters.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=lintcode id=386 lang=cpp
+ *
+ * [386] Longest Substring with At Most K Distinct Characters
+ *
+ * https://www.lintcode.com/problem/longest-substring-with-at-most-k-distinct-characters/description
+ *
+ * lintcode
+ * Medium (28.00%)
+ * Total Accepted:    14619
+ * Total Submissions: 52156
+ * Testcase Example:  '"eceba"\n3'
+ *
+ * Given a string *S*, find the length of the longest substring *T* that
+ * contains at most k distinct characters.
+ * 
+ * Example 1:
+ * Input: S = "eceba" and k = 3
+ * Output: 4
+ * Explanation: T = "eceb"
+ * 
+ * Example 2:
+ * Input: S = "WORLD" and k = 4
+ * Output: 4
+ * Explanation: T = "WORL" or "ORLD"
+ * Challenge: O(n) time
+ * 
+ * @Category HashTable + Sliding Window / two pointers
+ * @Idea
+ * S1: hashtable记字母频率 + 右进1(左进n)
+ * 
+ * 
+ */
+class Solution {
+public:
+    /**
+     * @param s: A string
+     * @param k: An integer
+     * @return: An integer
+     */
+    int lengthOfLongestSubstringKDistinct(string &s, int k) {
+        int res = 0, left = 0;                                  // 坑: ,而不是;
+        unordered_map<char, int> m;
+        for (int right = 0; right < s.size(); ++right) {        // 右边界+1扩大窗口
+            ++m[s[right]];                                          // 频率+1, 若m不含则新建s[i]
+            while (m.size() > k) {                              // 左边界不断+1缩小窗口直到窗口内满足条件
+                if (--m[s[left]] == 0) { m.erase(s[left]); }    // 频率-1, 若频率0则删除s[i]
+                ++left;
+            }
+            res = max(res, right - left + 1);                   // 更新最大窗口大小 // 坑: 勿忘right-left+1
+        }
+        return res;
+    }
+};
