6ec897d08b863f04d99db07fe5940b3e1b9e51b9
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-19 00:54:46 -0700

    update 386.longest-substring-with-at-most-k-distinct-characters.cpp

--- 386.longest-substring-with-at-most-k-distinct-characters.cpp
+++ 386.longest-substring-with-at-most-k-distinct-characters.cpp
@@ -39,32 +39,31 @@ public:
      * @return: An integer
      */
     // S1: hashtable记字母频率
-    // 窗口内满足条件hashtable.size()<=2, 左移使得 先右边界+1再左边界+n
+    // 窗口内满足条件hashtable.size()<=2, 左边界上出窗的字符若频率为0则从hashtable中删除
     // T= O(n) S=O(1)
-    int lengthOfLongestSubstringKDistinct1(string &s, int k) {
+    int lengthOfLongestSubstringKDistinct(string &s, int k) {
         int res = 0, left = 0;                                  // 坑: ,而不是;
         unordered_map<char, int> m;
         for (int right = 0; right < s.size(); ++right) {        // 右边界+1扩大窗口
-            ++m[s[right]];                                      // 频率+1, 若m不含会自动新建s[i]
+            ++m[s[right]];                                      // 更新频率+1, 若m不含会自动新建s[i]
             while (m.size() > k) {                              // 左边界不断+1缩小窗口直到满足条件
-                if (--m[s[left]] == 0) { m.erase(s[left]); }    // 频率-1, 若频率减为0则删除s[i]
+                if (--m[s[left]] == 0) { m.erase(s[left]); }    // 左边界上将出窗字符频率减为0,则出窗后需从m删除
                 ++left;
             }
             res = max(res, right - left + 1);                   // 更新最大窗口大小 // 坑:勿忘+1!!!
         }
         return res;
     }
-
     // S2: hashtable记字母最后出现位置
-    // 窗口内满足条件hashtable.size()<=2, 先右边界+1再左边界
+    // 窗口内满足条件hashtable.size()<=2, 左边界上将出窗的字符最后出现在该位置则出窗后需从hashtable删除
     // T= O(n) S=O(1)
-    int lengthOfLongestSubstringKDistinct(string &s, int k) {
+    int lengthOfLongestSubstringKDistinct2(string &s, int k) {
         int res = 0, left = 0;
         unordered_map<char, int> m;
-        for (int right = 0; right < s.size(); ++right) {
-            m[s[right]] = right;                                    // 更新s[right]字母最后出现的位置
+        for (int right = 0; right < s.size(); ++right) {        // 右边界+1扩大窗口
+            m[s[right]] = right;                                // 更新s[right]最后出现的位置
             while (m.size() > k) {                              // 左边界不断+1缩小窗口直到满足条件
-                if (m[s[left]] == left) { m.erase(s[left]); }   // 左边界上的字符最后出现在位置,出窗后需从m删除
+                if (m[s[left]] == left) { m.erase(s[left]); }   // 左边界上将出窗字符最后出现在该位置,则出窗后需从m删除
                 ++left;
             }
             res = max(res, right - left + 1);                   // 更新最大窗口大小 // 坑:勿忘+1!!!
