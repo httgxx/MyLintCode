6920907d530d23971ae3af43e002ee988b475329
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-22 01:39:52 -0700

    update 76.longest-increasing-subsequence.cpp

--- 76.longest-increasing-subsequence.cpp
+++ 76.longest-increasing-subsequence.cpp
@@ -36,7 +36,7 @@
  * 每次进新数x,就看它能否让某些长度的LIS的最小end变得更小
  * 若找到可以替换的end, 找到最小的那个替换为x; 若没找到,则直接append到ends末尾,即找到更长的LIS
  * 最后ends的长度也就是最长LIS的长度
-* 注: 若LIS不允许重复数,则ends不允许有重复数;如果LIS允许重复数,则ends允许有重复数
+ * 注: 若LIS不允许重复数,则ends不允许有重复数;如果LIS允许重复数,则ends允许有重复数
  *    两种情况下都是用a[i]去替换第一个>=a[j]的ends[k]
  * 
  * S2: // T=O(n^2) S=O(n)
@@ -57,22 +57,32 @@ public:
      * @param nums: An integer array
      * @return: The length of LIS (longest increasing subsequence)
      */
-    // S1: DP T=O(nlogn) S=O(n)
-    // 同长LIS中end越小越可能append新数成为更长LIS,故记录每种长度的LIS的最小end即ends[i]=长度为i的LIS的最小end
+    // S1: DP ends[] // T=O(nlogn) S=O(n)
+    // 同长LIS中end越小越可能append新数成为更长LIS,故记录每种长度的LIS的最小end即ends[i]=长度为(i+1)的LIS的最小end
     // 每次进新数x,就看它能否让某些长度的LIS的最小end变得更小
     // 若找到可以替换的end, 找到最小的那个替换为x; 若没找到,则直接append到ends末尾,即找到更长的LIS
     // 最后ends的长度也就是最长LIS的长度
-    int longestIncreasingSubsequence(const vector<int> &nums) {
-        vector<int> ends;                                       // ends[i]=长度为(i-1)的LIS的最小end
+    int longestIncreasingSubsequence1(const vector<int> &nums) {
+        vector<int> ends;                                       // ends[i]=长度为(i+1)的LIS的最小end
         for (int i = 0; i < nums.size(); ++i) {                 // 每访问一个新数
             int left = 0, right = ends.size();                  // 坑: 必须每次重新计算ends.size(),因为ends可能已变
-            while (left < right) {                              // 二分查找ends中第1个>=新数
+            while (left < right) {                              // 二分查找ends中第1个>=新数的值
                 int mid = left + (right - left) / 2;
                 if (ends[mid] < nums[i]) { left = mid + 1; }    // 要找>=新数的最小值,[mid]<新数,不够大,故去右边找更大
                 else {  right = mid; }                          // [mid]>=新数符合条件,继续去左边(含mid)找是否有更小的符合条件
             }
             if (right < ends.size()) { ends[right] = nums[i]; } // 若找到,则用新数替换它,使其对应长度的LIS的最小end变得更小(更可能以后形成更长LIS)
             else { ends.push_back(nums[i]); }                   // 若没找到,则加到ends尾,表明发现了更长的LIS,其最小end是新数
+        }
+        return ends.size();                                     // ends[0]=长度为0+1的LIS的最小end => ends[last]对应长度为last+1=size的LIS
+    }
+    // S1.1 DP ends[] + lower_bound // T=O(nlogn) S=O(n)
+    int longestIncreasingSubsequence(const vector<int> &nums) {
+        vector<int> ends;                                       // ends[i]=长度为(i+1)的LIS的最小end
+        for (auto a : nums) {                                   // 每访问一个新数
+            auto it = lower_bound(ends.begin(), ends.end(), a); // 找到ends中第1个>=新数的值
+            if (it == ends.end()) { ends.push_back(a); }        // 若没找到,则加到ends尾,表明发现了更长的LIS,其最小end是新数
+            else { *it = a; }                                   // 若找到,则用新数替换它,使其对应长度的LIS的最小end变得更小(更可能以后形成更长LIS)
         }
         return ends.size();                                     // ends[0]=长度为0+1的LIS的最小end => ends[last]对应长度为last+1=size的LIS
     }
