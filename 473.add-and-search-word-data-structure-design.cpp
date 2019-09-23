534f51cf9b0adb96fc2063b7c3d42858dfcb2092
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-23 00:24:33 -0700

    update 473.add-and-search-word-data-structure-design.cpp

--- 473.add-and-search-word-data-structure-design.cpp
+++ 473.add-and-search-word-data-structure-design.cpp
@@ -50,10 +50,10 @@
  * bool searchHelper(word, parent, idx)要在parent的child中找w[idx]
  * 必须传parent而不能传child
  * 
- * 坑:别忘初始化root!!!
+ * 坑:别忘WordDictionary()初始化root!!!
+ * 坑:for(auto &c:child) 勿忘&不然不改变!!!
  * 
  */
-
 class WordDictionary {
 public:
     struct TrieNode {
@@ -61,50 +61,52 @@ public:
         TrieNode *child[26];
         bool isWord;
         TrieNode() : isWord(false) {
-            for (auto &a : child) a = NULL;
+            for (auto &c : child) { c = NULL; }  // 手动全赋NULL // 坑: 勿忘&不然不改变!!!
         }
     };
-    
+
     WordDictionary() {
-        root = new TrieNode();
+        root = new TrieNode();                  // 坑:别忘初始化root!!!
     }
-    
-    // Adds a word into the data structure.
-    void addWord(string word) {
+
+    /*
+     * @param word: Adds a word into the data structure.
+     * @return: nothing
+     */
+    void addWord(string &word) {
         TrieNode *p = root;
-        for (auto &a : word) {
-            int i = a - 'a';
-            if (!p->child[i]) p->child[i] = new TrieNode();
+        for (auto c : word) {
+            int i = c - 'a';
+            if (!p->child[i]) { p->child[i] = new TrieNode(); }
             p = p->child[i];
         }
         p->isWord = true;
     }
 
-    // Returns if the word is in the data structure. A word could
-    // contain the dot character '.' to represent any one letter.
-    bool search(string word) {
-        return searchWord(word, root, 0);
+    /*
+     * @param word: A word could contain the dot character '.' to represent any one letter.
+     * @return: if the word is in the data structure.
+     */
+    bool search(string &word) {
+        return searchHelper(word, root, 0);                     // search(w,parent,idx)在parent的儿子里找w[idx]
     }
-    
-    bool searchWord(string &word, TrieNode *p, int i) {
-        if (i == word.size()) return p->isWord;
-        if (word[i] == '.') {
-            for (auto &a : p->child) {
-                if (a && searchWord(word, a, i + 1)) return true;
+
+    bool searchHelper(string &word, TrieNode *parent, int i) {  // i是当前word中待match的字符的index
+        if (i == word.length()) { return parent->isWord; }      // 整个word都match完
+        if (word[i] == '.') {                                   // 遇到'.'
+            for (auto &c : parent->child) {                     // 在每个儿子的儿子中找word的下个字符
+                if (c && searchHelper(word, c, i + 1)) {        // 递归匹配下个字符
+                    return true;                                // 从递归最底层返回true,继续返回true
+                }
             }
-            return false;
-        } else {
-            TrieNode * t = p->child[word[i] - 'a'];
-            return t && searchWord(word, t, i + 1);
+            return false;                                       // 每个儿子的支路都匹配失败,返回false
+        }
+        else {
+            TrieNode *t = parent->child[word[i] - 'a'];
+            return t && searchHelper(word, t, i + 1);           // 在当前匹配的儿子的儿子中找word的下个字符
+                                                                // 从递归最底层返回的结果直接返回上1层
         }
     }
-    
 private:
     TrieNode *root;
 };
-
-// Your WordDictionary object will be instantiated and called as such:
-// WordDictionary wordDictionary;
-// wordDictionary.addWord("word");
-// wordDictionary.search("pattern");
-
