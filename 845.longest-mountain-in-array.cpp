c5fc925f5c432f6b8c27d441e906053c8ed6f8bd
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-24 02:43:44 -0700

    update 845.longest-mountain-in-array.cpp

--- 845.longest-mountain-in-array.cpp
+++ 845.longest-mountain-in-array.cpp
@@ -38,25 +38,46 @@
  *
  * Follow up: solve in 1 pass 且 S=O(1)
  * 
+ * @Category 单调性
+ * @Idea T=O(n) S=O(1) 
+ * S1: 找上山起点,找山顶,找下山终点,更新最长山长,再从下山终点作为新起点,重复进行
+ * S2: up=上山长,down=下山长,到山底(down&&[i]>[i-1])或遇到平地([i]==[i-1])时清零up和down,
+ *     上山([i]>[i-1])则++up,下山([i]<[i-1])则++down, up>0&&down>0时才用(up+down+1)更新res
  * 
- * 
+ * 坑: 从[1]而不是[0]开始,因为要查是否上山要和前一个比
+ * 坑: 若到山底(down时发现开始递增)或遇平地,则清零up和down
+ * 坑: 在上山和下山的过程中不断更新最长山长,更新要用(up+down+1山顶)
  */
 class Solution {
 public:
-    // S1: 找上山起点,找山顶,找下山终点,更新最长山长,终点成新起点循环进行
-    int longestMountain(vector<int>& A) {
+    // S1: 找上山起点,找山顶,找下山终点,更新最长山长,再从下山终点作为新起点,重复进行
+    // T=O(n) S=O(1)
+    int longestMountain1(vector<int>& A) {
         int res = 0, i = 0, last = A.size() - 1;
         while (i <= last - 2) {                                             // 坑: 最短山长=3 起点至少在倒数第3
-            while (i <= last - 2 && A[i] >= A[i + 1]) { ++i; }             // 找上山起点: 跳过所有不增
+            while (i <= last - 2 && A[i] >= A[i + 1]) { ++i; }              // 找上山起点: 跳过所有不增
             int peak = i;
-            while (peak <= last - 1 && A[peak] < A[peak + 1]) { ++peak; }  // 找山顶: 统计所有递增
+            while (peak <= last - 1 && A[peak] < A[peak + 1]) { ++peak; }   // 找山顶: 统计所有递增
             int j = peak;
-            while (j <= last - 1 && A[j] > A[j + 1]) { ++j; }              // 找下山终点: 统计所有递减
+            while (j <= last - 1 && A[j] > A[j + 1]) { ++j; }               // 找下山终点: 统计所有递减
             if (i < peak && peak < j) { res = max(res, j - i + 1); }        // 坑: 千万别忘 i<peak<j
             i = j;                                                          // 坑: 千万别忘 i=j
         }
         return res;
     }
 
-    // S
+    // S2: 用up和down分别记录上山和下山的长度,遇到平地或者下到山底则清零up和down,若up和down都>0则用up+down更新最长山长
+    // T=O(n) S=O(1)
+    int longestMountain(vector<int>& A) {
+        int res = 0, up = 0, down = 0, n = A.size();
+        for (int i = 1; i < n; ++i) {                               // 坑: 从[1]而不是[0]开始,因为要查是否上山要和前一个比
+            if ((down && A[i - 1] < A[i]) || (A[i - 1] == A[i])) {  // 若到山底(down时发现开始递增)或遇平地,则清零up和down
+                up = down = 0;
+            }
+            if (A[i - 1] < A[i]) { ++up; }                          // 否则, 要么在上山, 更新up
+            if (A[i - 1] > A[i]) { ++down; }                        // 要么在下山, 更新down
+            if (up > 0 && down > 0) res = max(res, up + down + 1);  // 在上山和下山的过程中不断更新最长山长 // 坑: +1算山顶
+        }
+        return res;
+    }
 };
