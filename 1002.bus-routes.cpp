5c5b6ce114be5668961f0cce1d1ff1c53d44305b
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-26 01:26:19 -0700

    update 1002.bus-routes.cpp

--- 1002.bus-routes.cpp
+++ 1002.bus-routes.cpp
@@ -35,13 +35,10 @@
  * 
  * @Category 迷宫最短路径 BFS
  * @Idea
- * 求从站S到站T要坐的公交数而不是站数,每个公交在不同站可换不同公交
- * 图:点=站,边=站->站,即:站->站能搭乘的公交->公交能到的下1站
- * 求站S到站T要换乘的最少公交数 = 各种公交换乘方案中,公交换乘次数最少的那个 
- * BFS分层遍历,每层换一辆公交(每坐过的才换,以前坐过的不坐,因为那代表坐过的那车不换也能达到当前站),求终点站到达时的最小层数
- * 递归终止条件=当前公交能到达终点
- * 要找一个公交下一步能换哪些其他的公交(找邻居),公交->站->公交,得有公交->站和站->公交这2个map
- * 
+ * 图:点={站},边={站->站} 站=>站上能搭乘的公交=>公交能到的下1站
+ * 求站S到站T要换乘的最少公交数 = 各种公交换乘方案中,公交换乘次数最少的那个(公交数最少而不是站最少!!)  
+ * BFS分层遍历,每层从1个站通过换1个没坐过的公交(坐过表明从那车可直接达到当前站),到达目标站时终止遍历
+ * 站->公交->站,所以既要公交->站的map(已给routes),也要站->公交的map(需要根据前者创建)
  */
 class Solution {
 public:
@@ -51,6 +48,9 @@ public:
      * @param T: destination
      * @return: the least number of buses we must take to reach destination
      */
+    // 图:点={站},边={站->站} 站=>站上能搭乘的公交=>公交能到的下1站
+    // 求站S到站T要换乘的最少公交数 = 各种公交换乘方案中,公交换乘次数最少的那个(公交数最少而不是站最少!!)  
+    // S1:BFS分层遍历,每层从1个站通过换1个没坐过的公交(坐过表明从那车可直接达到当前站),到达目标站时终止遍历
     int numBusesToDestination(vector<vector<int>> &routes, int S, int T) {
         if (S == T) { return 0; }                           // 特例
         unordered_map<int, vector<int>> stop2bus;           // 创建站->公交的map
@@ -64,12 +64,12 @@ public:
         queue<int> q{{S}};
         while (!q.empty()) {
             ++res;
-            for (int i = q.size(); i > 0; --i) {            // 从当前每个站出发扩散一步
+            for (int i = q.size(); i > 0; --i) {            // 从当前层每个站通过换乘1个公交进入下一层
                 int stop = q.front(); q.pop();
                 for (int bus : stop2bus[stop]) {            // 站(当前层)->公交
                     if (visitedBus.count(bus)) { continue; }//   跳过已访问过的公交
                     visitedBus.insert(bus);                 //   标记公交为已访问过
-                    for (int stop : routes[bus]) {          //公交->站(下一层)
+                    for (int stop : routes[bus]) {          // 公交->站(下一层)
                         if (stop == T) { return res; }      //   若到达目标站,结束遍历
                         q.push(stop);                       //   否则站入队列作为下一层继续遍历
                     }
