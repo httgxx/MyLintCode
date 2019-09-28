cf6f29b6cceeff059b6949da8c62bb88e8f12569
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-27 22:37:16 -0700

    update 363.trapping-rain-water.cpp

--- 363.trapping-rain-water.cpp
+++ 363.trapping-rain-water.cpp
@@ -41,16 +41,31 @@ public:
      * @param heights: a list of integers
      * @return: a integer
      */
-    // S1: 双指针
+    // S1: 双指针+木头原理:用低的板更新水平面 T=O(n) S=O(1)
     int trapRainWater1(vector<int> &heights) {
-        // write your code here
+        int res = 0, l = 0, r = heights.size() - 1;
+        while (l < r) {
+            int mn = min(heights[l], heights[r]);       // 找低的一边
+            if (heights[l] == mn) {                     // 若左低
+                ++l;                                    // 左指针往中间移1
+                while (l < r && heights[l] < mn) {      // 从左往右累计存水量,直到碰壁
+                    res += mn - heights[l++];           // l++先计算再右移
+                }
+            } else {                                    // 若右低,从右往左扫
+                --r;                                    // 左指针往中间移1
+                while (l < r && heights[r] < mn) {       // 从右往左累计存水量,直到碰壁
+                    res += mn - heights[r--];            // r--先计算再左移
+                }
+            }
+        }
+        return res;
     }
 
-    // S2: 木头原理:用低的板更新水平面 T=O(n) S=O(1)
+    // S2: (精简代码)双指针+木头原理:用低的板更新水平面 T=O(n) S=O(1)
     int trapRainWater(vector<int>& height) {
         int l = 0, r = height.size() - 1, level = 0, res = 0;
         while (l < r) {
-            int lower = height[(height[l] < height[r]) ? l++ : r--];    // 取低的那边,指针向中间移(木桶原理)
+            int lower = height[(height[l] < height[r]) ? l++ : r--];    // 取低的那边,指针向中间移1(木桶原理)
             level = max(level, lower);                                  // 用低的那边更新当前水平面
             res += level - lower;                                       // 计算可多存的水量
         }
