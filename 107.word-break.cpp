ec0b1b30f56239d2d0265d218bb507fab7a1e506
httgxx <42990730+httgxx@users.noreply.github.com>
2019-09-29 00:01:28 -0700

    update 107.word-break.cpp

--- 107.word-break.cpp
+++ 107.word-break.cpp
@@ -45,23 +45,24 @@ public:
      * @param dict: A dictionary of words dict
      * @return: A boolean
      */
-    // DP T=O(n^2) S=O(n]) TLE
+    // DP T=O(n^2) S=O(n])
+    // 坑: 必须用字典中最长单词长度剪枝,否则TLE!!!
     bool wordBreak(string &s, unordered_set<string> &dict) {
         if (s.empty()) { return true; }                             // 特例:空串
         int maxLen = INT_MIN;                                       // 找到字典中最长单词长度
         unordered_set<string>::iterator it;
         for (it = dict.begin(); it != dict.end(); ++it) {
-            maxLen = max(maxLen, (int)(*it).size());
+            maxLen = max(maxLen, (int)(*it).size());                // 坑: 必须加(int)
         }
         int n = s.length();
         bool canSegments[n];
         for (int i = 0; i < n; ++i) {                               // 顺着枚举开头字符
             canSegments[i] = false;                                 // 初始=不能拆分
-            for (int j = i; j >= 0 && i - j + 1 <= maxLen; --j) {   // 倒着枚举结尾字符,且[j,i]长度没超过字典中最长单词长度
-                if (j > 0 && canSegments[j - 1] == false) {         
+            for (int j = i; j >= 0 && i - j + 1 <= maxLen; --j) {   // 倒着枚举结尾字符,且[j,i]长度没超过字典中最长单词长度(否则TLE)
+                if (j > 0 && canSegments[j - 1] == false) {         // 坑:必须加j>0,且dp[j-1]==true即[0,j-1]可拆分
                     continue;
                 }
-                if (dict.count(s.substr(j, i - j + 1))) {
+                if (dict.count(s.substr(j, i - j + 1))) {           // 找到j使[0,j-1]可拆分且[j,i]在字典里则[0,j]可拆分
                     canSegments[i] = true;
                     break;
                 }
