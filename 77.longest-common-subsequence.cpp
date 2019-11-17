dfdb42f05d94bdffe96ec9e1c4fcf69c63a21d09
httgxx <42990730+httgxx@users.noreply.github.com>
2019-11-17 13:05:17 -0800

    update 77.longest-common-subsequence.cpp

--- 77.longest-common-subsequence.cpp
+++ 77.longest-common-subsequence.cpp
@@ -25,15 +25,21 @@
  * S1: DP T=O(mn) S=O(mn)
  * 特例: if A.empty()||B.empty() return 0
  * dp[i][j]表示A[0,i)和B[0,j)的LCS
- * dp[i][j]= 
- *   if A[i-1]==B[j-1] 1+dp[i-1][j-1]
- *   else max(dp[i-1][j],dp[i][j-1])
+ * =if(A[i-1]==B[j-1]) 1+dp[i-1][j-1]
+ *  else max(dp[i-1][j],dp[i][j-1])
  * 初始化 dp[0][j]=dp[i][0]=0
+ * 从i=1,j=1开始循环处理
  * 
  * 坑 特例: if A.empty()||B.empty() return 0
  * 坑 初始化 dp[0][j]=dp[i][0]=0
  * 
- * S2: Recursion
+ * S2: 递归+记忆搜索
+ * T=O(mn) S=O(mn)
+ * helper(subLenA, subLenB, mem)=
+ *     if A[subLenA-1]==B[subLenB-1]: 1+helper(subLenA-1,subLenB-1)
+ *     else: max(helper(subLenA-1,subLenB), helper(subLenA, subLenB-1))
+ * 坑1: 初始化mem所有为-1而非0来区分没计算过的情况和不匹配的情况
+ * 坑2: [al+1][bl+1]因mem[0][i]=0指长为0的A子串(而非首字符A[0])不匹配B
  */
 class Solution {
 public:
@@ -43,20 +49,44 @@ public:
      * @return: The length of longest common subsequence of A and B
      */
     // S1: DP 双序列 T=O(mn) S=O(mn)
-    int longestCommonSubsequence(string &A, string &B) {
+    int longestCommonSubsequence1(string &A, string &B) {
         if (A.empty() || B.empty()) { return 0; }               // 坑: 特例
         int al = A.length(), bl = B.length();
-        vector<vector<int>> dp(al + 1, vector<int>(bl + 1, 0)); // dp[i][j]表示A[0,i)和B[0,j)的LCS，初始化0
+        vector<vector<int>> dp(al + 1, vector<int>(bl + 1, 0)); // dp[i][j]表示A[0,i)和B[0,j)的LCS,(m+1)*(n+1),初始化=0
         for (int i = 1; i <= al; ++i) {
             for (int j = 1; j <= bl; ++j) {                     
                 if (A[i - 1] == B[j - 1]) {                     // 若最后字符同, 则最后字符构成新LCM
                     dp[i][j] = 1 + dp[i - 1][j - 1];            //   dp[i][j]=1+dp[i-1][j-1]
                 }
-                else {                                          // 若最后字符不同,则去掉A[i]或B[j]
+                else {                                          // 若最后字符不同,则去掉A[i]或B[j]求最大
                     dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]); //   dp[i][j]=max(dp[i][j-1],dp[i-1][j])
                 }
             }
         }
-        return dp[al][bl];                                      // 返回dp[m-1][n-1]
+        return dp[al][bl];                                      // 返回dp[m][n]
+    }
+
+    // S2: 递归+记忆搜索
+    // T=O(mn) S=O(mn)
+    int longestCommonSubsequence(const string& A, const string& B) {
+        if (A.empty() || B.empty()) { return 0; }               // 坑: 特例
+        int al = A.length(), bl = B.length();
+        vector<vector<int>> m(al + 1, vector<int>(bl + 1, -1)); // 记忆m[i][j]=A[0,i]和B[0,j]的LCS 坑:al+1,bl+1
+                                                                // 坑1: 须初始化所有为-1而非0来区分没计算过的情况和不匹配的情况
+                                                                // 坑2: 须[al+1][bl+1]因mem[0][i]=0指长为0的A子串(而非首字符A[0])不匹配B
+        for (int i = 0; i <= al; ++i) { m[i][0] = 0; }          // 初始化首行首列m[0][j]=m[i][0]=0
+        for (int j = 0; j <= bl; ++j) { m[0][j] = 0; }
+        return helper(A, al - 1, B, bl - 1, m);                 // 递归+记忆搜索
+    }
+    int helper(const string &A, int endA, const string &B, int endB, vector<vector<int>>& m) {
+        if(m[endA + 1][endB + 1] >= 0) {
+            return m[endA + 1][endB + 1];                       // 若有记忆,直接返回记忆值
+        }        
+        m[endA + 1][endB + 1] =                                 // 若无记忆,计算并更新记忆    
+            A[endA] == B[endB]
+            ? 1 + helper(A, endA - 1, B, endB - 1, m)           //   若最后同, 则最后字符构成新LCM
+            : max(helper(A, endA - 1, B, endB, m),              //   若最后不同,则去掉A[i]或B[j]求最大
+                  helper(A, endA, B, endB - 1, m));
+        return m[endA + 1][endB + 1];
     }
 };
