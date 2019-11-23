2fdd5f3106d07e69d7f8e5fef23393b89b3df48f
httgxx <42990730+httgxx@users.noreply.github.com>
2019-11-23 03:01:14 -0800

    update 131.the-skyline-problem.cpp

--- 131.the-skyline-problem.cpp
+++ 131.the-skyline-problem.cpp
@@ -50,6 +50,12 @@
  * ]
  * Explanation:
  * The buildings are look like this in the picture. The yellow part is buildings.
+ * 
+ * @Category skyline
+ * @Idea 扫描线  T=O(nlogn) S=O(n)
+ * 先把每个[start,end,height]打散成[start,-h],[end,h],按x轴从小到大排,则同h的start在end前
+ * 从左扫到右,若start(-h<0)则h入堆,若end(h>0)则h出堆,退出后将余下高度最大值入结果集
+ * 需将拐点转换成三元组加入结果集
  */
 class Solution {
 public:
@@ -57,37 +63,36 @@ public:
      * @param buildings: A list of lists of integers
      * @return: Find the outline of those buildings
      */
-    vector<vector<int>> buildingOutline(vector<vector<int>> &buildings) {
-        // write your code here
-        vector<vector<int>> ret;
-        vector<pair<int, int>> nodes;
-        for (auto &building: buildings) {
-            nodes.emplace_back(building[0], -building[2]); // 加入先放大的高度
-            nodes.emplace_back(building[1], building[2]); // 退出先放小的高度 
+    // 先把每个[start,end,height]打散成[start,-h],[end,h],按x轴从小到大排,则同h的start在end前
+    // 从左扫到右,若start(-h<0)则h入堆,若end(h>0)则h出堆,退出后将余下高度最大值入结果集
+    // 需将拐点转换成三元组加入结果集
+    // T=O(nlogn) S=O(n)
+    vector<vector<int>> buildingOutline(vector<vector<int>>& buildings) {
+        if(buildings.empty()) { return {}; }    // 特例:空串
+        vector<vector<int>> res, xh;
+        for (auto &b : buildings) {             // 每个[start,end,h]打散成[start,-h],[end,h]
+            xh.push_back({b[0], -b[2]});
+            xh.push_back({b[1], b[2]});
         }
+        sort(xh.begin(), xh.end());             // 按x坐标从小到大排序,同高start在end前(-h<h)
         
-        sort(nodes.begin(), nodes.end());
-        
-        multiset<int> m;
-        m.insert(0);
-        int cur_h = 0, prev_h = 0;
-        pair<int, int> prev_pt = {0, 0};
-        
-        for (auto &node: nodes) {
-            if (node.second < 0) {
-                m.insert(-node.second);                
-            } else {
-                m.erase(m.find(node.second));
-            }
-            cur_h = *m.rbegin();
-            if (cur_h != prev_h) {
-                if (prev_pt.second != 0) {
-                    ret.push_back({prev_pt.first, node.first, prev_pt.second});
+        multiset<int> m;                        // multiset(按堆排好序,插入删除后仍有序)代替最大堆存h
+        m.insert(0);                            // 巧: 加0以便将没和其他楼重叠的end也加入结果集合
+        int pre = 0, cur = 0;                   // 上一高度,当前高度,都初始为0
+        vector<int> pre_pt = {0, 0};            // 上一拐点[x,h]
+        for (auto &a : xh) {
+            if (a[1] < 0) { m.insert(-a[1]); }  // 若start,进入楼,则h入堆m
+            else { m.erase(m.find(a[1])); }     // 若end,退出楼,则h出堆m
+            cur = *m.rbegin();                  // 当前高度=m中最大h(尾元素)
+            if (cur != pre) {                   // 若计算的当前高度!=上一高度
+                if (pre_pt[1] != 0) {           // [x,当前高度]入结果集
+                    res.push_back({pre_pt[0], a[0], pre_pt[1]});
                 }
-                prev_pt = {node.first, cur_h};
-                prev_h = cur_h;
+                pre_pt[0] = a[0];
+                pre_pt[1] = cur;
+                pre = cur;                      //   当前高成为新的上一高度    
             }
         }
-        return ret;
+        return res;
     }
 };
